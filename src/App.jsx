db8b6ba4faea48f0e9f17acd6f5640e24d8427db
Facundo David Gonzalez <facundo.gonzalez@devactory.com>
2022-11-04 01:57:02 -0300

    add chakra-ui

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,11 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import { Container } from '@chakra-ui/react'
+import { Button } from '@chakra-ui/react'
 
-function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank" rel="noreferrer">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
-  )
-}
+const App = () => (
+  <Container maxW='container.xl' bg='blue.600' color='white'>
+    <h1>Movie Catalog</h1>
+    <Button colorScheme='blue'>Button example</Button>
+  </Container>
+)
 
 export default App
